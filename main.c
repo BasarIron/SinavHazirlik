a50f29ac30cda4149c4af7c2b9ad4d46e739ae36
BasarIron <basar151516@gmail.com>
2025-01-08 19:28:46 +0300

    c dosyasi guncellendı

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
+int i=0;
+
 int main(void){
     int sayi;
     printf("hello world");
     scanf("%d",&sayi);
+        for(i=0;i<sayi;i++){
+            printf("%d. sayiyi girinizz..\n",i);
+        }
     return 0;
 }
